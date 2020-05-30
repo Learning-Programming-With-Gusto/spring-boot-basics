9f93c0feeccc96d2009bc2b12145a3ce4ac15423
Chad Brooks <cbchd@netscape.net>
2020-05-30 12:29:59 -0400

    -

--- Backend-Server/server/src/main/java/com/specialtimes/server/controllers/PotatoController.java
+++ Backend-Server/server/src/main/java/com/specialtimes/server/controllers/PotatoController.java
@@ -1,17 +1,32 @@
 package com.specialtimes.server.controllers;
 
 import com.specialtimes.server.models.Potato;
+import com.specialtimes.server.services.PotatoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@RestController(value = "/potatoes")
 public class PotatoController {
 
-    @RequestMapping("/potatoes")
+    private PotatoService potatoService;
+
+    @Autowired
+    public PotatoController(PotatoService potatoService) {
+        this.potatoService = potatoService;
+    }
+
+    @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<Potato>> getAllPotatoes() {
-        return getAllPotatoes();
+        return potatoService.getAllPotatoes();
+    }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<List<Potato>> addPotato() {
+        return potatoService.getAllPotatoes();
     }
 }
